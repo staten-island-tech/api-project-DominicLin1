cf71519528a776893ef46f20a2d9b30013ece621
DominicLin1 <dominiclin777@gmail.com>
2023-01-20 10:16:27 -0500

    c

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,6 +1,20 @@
 import "../css/style.css"
 const URL= "https://animechan.vercel.app/api/random"
-fetch('https://animechan.vercel.app/api/random')
+const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=naruto"
+/* fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
-    .then(quote => console.log(quote))
-
+    .then(quote => console.log(quote));
+ */
+    async function getData(URL) {
+   
+   try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    document.getElementById("api").textContent=data
+    console.log(data);
+   } catch (error) {
+    console.log(error)
+   }
+       
+    }
+getData(URL);
