8f791f8a5fbb5235f43b1a11215cb1507f37b5d1
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-02-05 17:05:15 -0500

    commit

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,25 +1,36 @@
 import "../css/style.css"
 const URL= "https://animechan.vercel.app/api/random"
-const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${}"
+const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${title}"
 const DOMselectors= {
-   form: document.getElementById("form")
+   form: document.getElementById("form"),
+   title:document.getElementById("title"),
 }
 /* fetch('https://animechan.vercel.app/api/random')
     .then(response => response.json())
     .then(quote => console.log(quote));
  */
 
-   DOMselectors.form.addEventListener("submit")
-    async function getData(AnimeTitleURL) {
+   DOMselectors.form.addEventListener("submit",
+function create(event){
+   insert(event);
+   event.preventDefault();
+}
+   )
+   function insert(){
+      let title=document.title.value
+      getData(title)
    
+   }
+    async function getData(AnimeTitleURL) {
+     
    try {
+    
     const response = await fetch(AnimeTitleURL);
     const data = await response.json();
     document.getElementById("api").textContent=data
     console.log(data);
+    console,log(response)
    } catch (error) {
     console.log(error)
    }
-       
-    }
-getData(AnimeTitleURL);
+       }
