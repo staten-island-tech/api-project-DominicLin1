635a03257d5839af2397f9056b692b6c948f1435
DominicLin1 <dominiclin777@gmail.com>
2023-02-07 10:14:47 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,6 +1,6 @@
 import "../css/style.css"
 const URL= "https://animechan.vercel.app/api/random"
-const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${title}"
+//const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${title}"
 const DOMselectors= {
    form: document.getElementById("form"),
    title:document.getElementById("title"),
@@ -21,15 +21,15 @@ function create(event){
       getData(title)
    
    }
-    async function getData(AnimeTitleURL) {
-     
+    async function getData(e) {
+      let AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${e}"
    try {
     
     const response = await fetch(AnimeTitleURL);
     const data = await response.json();
     document.getElementById("api").textContent=data
     console.log(data);
-    console,log(response)
+    console.log(response)
    } catch (error) {
     console.log(error)
    }
