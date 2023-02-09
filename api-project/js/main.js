155fa98b1442fd5b1d407d5602c834784b1dcb7f
DominicLin1 <113190169+DominicLin1@users.noreply.github.com>
2023-02-09 13:58:46 -0500

    Update main.js

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,14 +1,9 @@
 import "../css/style.css"
-const URL= "https://animechan.vercel.app/api/random"
-//const AnimeTitleURL="https://animechan.vercel.app/api/random/anime?title=${title}"
 const DOMselectors= {
    form: document.getElementById("form"),
    title:document.getElementById("title"),
 }
-/* fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote));
- */
+
 
    DOMselectors.form.addEventListener("submit",
 function create(event){
